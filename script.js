9ffe6af71ba0d8e201b2044160919fa608d1506a
Laura Baukol <laura.baukol@mac.com>
2016-10-14 16:05:02 -0600

    mouseover, mouseout and click functions

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+"use strict";
 /*
   Write JS to make this stoplight work.
 
@@ -8,3 +9,47 @@
   When I click on the 'go' button
     the bottom light should turn green.
 */
+var enterStop = document.querySelector("#stopLight");
+enterStop.addEventListener("mouseover", function() {
+  console.log("Entered Stop button");
+});
+
+var leaveStop = document.querySelector("#stopLight");
+leaveStop.addEventListener("mouseout", function() {
+  console.log("Left Stop button");
+});
+
+var clickStop = document.querySelector("#stopLight");
+clickStop.addEventListener("click", function() {
+   clickStop.style.backgroundColor = "red";
+});
+
+var enterSlow = document.querySelector("#slowLight");
+enterSlow.addEventListener("mouseover", function() {
+  console.log("Entered Slow button");
+});
+
+var leaveSlow = document.querySelector("#slowLight");
+leaveSlow.addEventListener("mouseout", function() {
+  console.log("Left Slow button");
+});
+
+var clickSlow = document.querySelector("#slowLight");
+clickSlow.addEventListener("click", function() {
+   clickSlow.style.backgroundColor = "orange";
+});
+
+var enterGo = document.querySelector("#goLight");
+enterGo.addEventListener("mouseover", function() {
+  console.log("Entered Go button");
+});
+
+var leaveGo = document.querySelector("#goLight");
+leaveGo.addEventListener("mouseout", function() {
+  console.log("Left Go button");
+});
+
+var clickGo = document.querySelector("#goLight");
+clickGo.addEventListener("click", function() {
+   clickGo.style.backgroundColor = "green";
+});
